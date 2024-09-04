ceb0368fb18a48420af92ab93eb2f8453d2f59c1
Leon Linß <leon.linss@schrauben-niro.de>
2024-09-04 13:41:51 +0200

    AdminPanel angepasst, Überschriften korrigiert und Filter für Produkte hinzugefügt

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,11 +5,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\NumericFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -18,13 +23,22 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
+    // Set the title of the page
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle(Crud::PAGE_INDEX, 'Manage Products') // Set title for the index page
+            ->setPageTitle(Crud::PAGE_EDIT, 'Edit Product') // Set title for the edit page
+            ->setPageTitle(Crud::PAGE_NEW, 'Create New Product'); // Set title for the new product page
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('name'),
             MoneyField::new('price')
-                ->setCurrency('EUR') // Stellen Sie sicher, dass die Währung richtig gesetzt ist
-                ->setStoredAsCents(false) // Falls Sie keine Cents benötigen
+                ->setCurrency('EUR')
+                ->setStoredAsCents(false)
                 ->setHelp('Enter the price in EUR'),
             TextareaField::new('description'),
             AssociationField::new('category'),
@@ -34,5 +48,14 @@ class ProductCrudController extends AbstractCrudController
                 ->setUploadedFileNamePattern('[randomhash].[extension]')
                 ->setRequired(false),
         ];
+    }
+
+    // Add filters for better search functionality
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(TextFilter::new('name')) // Filter by product name
+            ->add(NumericFilter::new('price')) // Filter by price
+            ->add(EntityFilter::new('category')); // Filter by category
     }
 }
